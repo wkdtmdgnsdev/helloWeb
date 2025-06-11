2ca2bec8580120c4657702dc64c28a7a27660e0c
wkdtmdgnsdev <wkdtmdgnsdev@gmail.com>
2025-06-11 11:35:06 +0900

    first commit

---
+++ src/main/java/org/kosa/hello/HelloController.java
@@ -0,0 +1,20 @@
+package org.kosa.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.ServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+	@GetMapping("/")
+	public String index(ServletRequest request) {
+		log.info("server port = {}", request.getServerPort());
+		return "Hello World..." +request.getServerPort();
+	}
+	
+	
+}
